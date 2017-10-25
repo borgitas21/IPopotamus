74dc48b06c0eacffbb47aa516b97b5d6f811658d
borja ruiz torres <borgitas21@users.noreply.github.com>
2017-10-25 21:37:14 +0200

    Prepare stylesheets structure

--- src/App.vue
+++ src/App.vue
@@ -11,15 +11,4 @@ export default {
 };
 </script>
 
-<style lang="sass" src="bulma"></style>
-
-<style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-</style>
+<style lang="sass" src="./assets/stylesheets/main.sass"></style>
